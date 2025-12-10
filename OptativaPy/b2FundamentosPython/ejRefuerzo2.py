b4bd65d14c068ed190745aa2552a5b099f54e49f
unknown <ccarveg3010@g.educaand.es>
2025-12-10 19:44:58 +0100

    Añadir mensajes de lo que debes de hacer

--- OptativaPy/b2FundamentosPython/ejRefuerzo2.py
+++ OptativaPy/b2FundamentosPython/ejRefuerzo2.py
@@ -1,7 +1,7 @@
 from functools import reduce
 # Ejemplo de entrada:
 texto = "Python es un lenguaje de programación increíble para ciencia de datos"
-
+# a partir de ahora hacer más funciones para más legiviidad
 # Salida esperada:
 #numero de palabras los espacios mas la primera palabra(esto no es fiable)busca otra solucion 
 #numPalabras=len([ i for i in texto if i==" " ])+1
@@ -13,10 +13,10 @@ print("Número de palabras: "+numPalabras) # Número de palabras: 11
 # Palabra más larga: programación (12 caracteres)
 caracteresDePalabraLarga = reduce(lambda a, b: a if len(a) > len(b) else b, texto.split())
 print("Palabra más larga: "+caracteresDePalabraLarga+"("+len(caracteresDePalabraLarga)+" caracteres)")
-# Vocales: a=6, e=8, i=5, o=4, u=2
+# Vocales: a=6, e=8, i=5, o=4, u=2 intenta implementar el reduce (mete parametros a las fucniones)
 vocales={"a":0,"e":0,"i":0,"o":0,"u":0}
- for i in texto.split():
+for i in texto.split():
     if i==" ":
         print(i) 
 
-# Top 3 palabras más largas: programación, increíble, lenguaje(mayor de 8 caracteres)
+# Top 3 palabras más largas: programación, increíble, lenguaje(mayor de 8 caracteres) ordenar por la longitud de la palabra
