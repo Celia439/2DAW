c36c2d0e7e32e367a987923fc577cc3dd695dcec
neko __pathatha <ccarveg3010@g.educaand.es>
2026-01-09 15:19:29 +0100

    un poquito de cliente

--- entorno cliente/js/UD4/ActividadesUD4_R1_CeliaCaravacaVega/js/ejercicio3.js
+++ entorno cliente/js/UD4/ActividadesUD4_R1_CeliaCaravacaVega/js/ejercicio3.js
@@ -2,15 +2,19 @@
 //1. Acceso al DOM usando querySelector():
 //Acceder a:
 //○ El primer plan usando un selector de clase.
-let plan1= document.querySelector(".plan");
+let plan1 = document.querySelector(".plan");
 //○ El botón usando un selector de id.
-let boton= document.getElementById("btnSeleccionar");
+let boton = document.getElementById("btnSeleccionar");
 //○ El primer elemento <h3> usando un selector de etiqueta.
-let h3Primero= document.getElementsByTagName("h3")[0];
-document.getElementById("resultado").innerHTML=h3Primero;
+let h3Primero = document.getElementsByTagName("h3")[0];
+document.getElementById("resultado").innerHTML = h3Primero;
 //2. Funcionalidad del botón:
 //Al hacer clic en el botón Seleccionar plan:
-//○ Añadir la clase CSS seleccionado únicamente al primer plan.
-plan1.addEventListener.apply(".plan");
-//○ Cambiar el texto del <h3> del plan seleccionado a: “Plan seleccionado”
-//○ Mostrar en el párrafo resultado el mensaje: “Has seleccionado el plan básico”
+boton.addEventListener("click", function () {
+  //○ Añadir la clase CSS seleccionado únicamente al primer plan.
+  plan1.classList.add(".seleccionado");
+  //○ Cambiar el texto del <h3> del plan seleccionado a: “Plan seleccionado”
+  h3Primero.textContent = "Plan seleccionado";
+  //○ Mostrar en el párrafo resultado el mensaje: “Has seleccionado el plan básico”
+  document.getElementById("resultado").textContent = "Has seleccionado el plan básico";
+});
