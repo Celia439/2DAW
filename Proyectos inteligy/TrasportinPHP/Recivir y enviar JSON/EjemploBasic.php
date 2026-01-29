006ddfea33587b1532e729cb32deef1637c5a5a2
unknown <ccarveg3010@g.educaand.es>
2026-01-29 20:45:45 +0100

    ejercicio js y scss killmepls

---
+++ Proyectos inteligy/TrasportinPHP/Recivir y enviar JSON/EjemploBasic.php
@@ -0,0 +1,8 @@
+<?php
+echo "Ejercicio 1: Crea un script PHP que defina la información de un alumno utilizando arrays
+PHP. El alumno deberá tener los siguientes datos:
+• DNI
+• Nombre
+• Edad
+• Un conjunto de asignaturas con su correspondiente nota
+Convierte la estructura creada a formato JSON y muéstrala por pantalla de forma legible.";
